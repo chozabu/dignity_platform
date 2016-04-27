31c2d385e967631b681bd1a48fb22c2b34180cd9
chozabu <chozabu@gmail.com>
2016-04-27 11:18:05 +0100

    use new bootstrap theme

--- dignity_platform/urls.py
+++ dignity_platform/urls.py
@@ -3,6 +3,9 @@ __author__ = "Alex 'Chozabu' P-B"
 __copyright__ = "Copyright 2016, Chozabu"
 
 from django.conf.urls import url
+from django.conf.urls.static import static
+
+import dignity.settings as settings
 
 from . import views
 
@@ -15,3 +18,13 @@ urlpatterns = [
     url(r'^causes/$', views.causes, name='causes'),
     url(r'^causes/(?P<cause_id>[0-9]+)/$', views.cause, name='cause'),
 ]
+
+theme_dir = settings.BASE_DIR + "/startbootstrap-creative-1.0.2"
+
+
+urlpatterns += static("/css/", document_root=theme_dir + "/css")
+urlpatterns += static("/fonts/", document_root=theme_dir + "/fonts")
+urlpatterns += static("/img/", document_root=theme_dir + "/img")
+urlpatterns += static("/js/", document_root=theme_dir + "/js")
+urlpatterns += static("/less/", document_root=theme_dir + "/less")
+urlpatterns += static("/font-awesome/", document_root=theme_dir + "/font-awesome")
