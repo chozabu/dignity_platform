7167f6457e93630c98d2c89c49ec1a5585d9b699
chozabu <chozabu@gmail.com>
2016-04-27 09:28:34 +0100

    add migration for jobhirer

--- dignity_platform/models.py
+++ dignity_platform/models.py
@@ -16,6 +16,26 @@ class Cause(models.Model):
 	def __str__(self):
 		return str(self.name)
 
+
+from django.db.models import F, Sum
+class CauseSupporterManager(models.Manager):
+	def balance_user_support(self, person, reserved=.0):
+		qs = self.get_queryset()
+		qs = qs.filter(person=person)
+		totalag = qs.aggregate(count=Sum('amount'))['count']
+		if reserved: totalag += reserved
+
+		qs.update(amount=F('amount') /totalag)
+		return qs
+
+class CauseSupporter(models.Model):
+	objects = CauseSupporterManager()
+	amount = models.FloatField(default=0.1)
+	person = models.ForeignKey(Person, related_name='causes_i_support')
+	cause = models.ForeignKey(Cause, related_name='people_supporting_cause')
+	def __str__(self):
+		return str(self.person) + " supports " + str(self.cause) + " at " + str(self.amount * 100) + "%"
+
 class Job(models.Model):
 	name = models.CharField(max_length=200)
 	def __str__(self):
